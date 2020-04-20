04bf30e53afc2319bd0811dc646500b94e20c42d
aahler <53316736+aahler@users.noreply.github.com>
2020-04-20 19:20:07 -0400

    header/homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, HomeComponent],
+  declarations: [AppComponent, LoginComponent, HomeComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,130 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  date: Date = new Date();
+  blogPosts: BlogPost[] = [
+    {
+      title: "Test title",
+      intro: "Test Intro",
+      content: "This is test content for the blog posts",
+      image: {
+        url: "",
+        description: "Description of the image",
+        alt: "Test picture",
+        filename: "Test Picture",
+      },
+      author: {
+        id: "12345",
+        name: "Greg",
+        avatar: "",
+      },
+      category: "Default",
+      published: true,
+      dateCreated: this.date,
+      datePublished: this.date,
+      lastModified: this.date,
+    },
+    {
+      title: "Test title 1",
+      intro: "Test Intro",
+      content: "This is test content for the blog posts",
+      image: {
+        url: "",
+        description: "Description of the image",
+        alt: "Test picture",
+        filename: "Test Picture",
+      },
+      author: {
+        id: "123456",
+        name: "Austin",
+        avatar: "",
+      },
+      category: "Default",
+      published: true,
+      dateCreated: this.date,
+      datePublished: this.date,
+      lastModified: this.date,
+    },
+    {
+      title: "Test title 2",
+      intro: "Test Intro",
+      content: "This is test content for the blog posts",
+      image: {
+        url: "",
+        description: "Description of the image",
+        alt: "Test picture",
+        filename: "Test Picture",
+      },
+      author: {
+        id: "1234567",
+        name: "Joey",
+        avatar: "",
+      },
+      category: "Default",
+      published: true,
+      dateCreated: this.date,
+      datePublished: this.date,
+      lastModified: this.date,
+    },
+    {
+      title: "Test title 3",
+      intro: "Test Intro",
+      content: "This is test content for the blog posts",
+      image: {
+        url: "",
+        description: "Description of the image",
+        alt: "Test picture",
+        filename: "Test Picture",
+      },
+      author: {
+        id: "12345678",
+        name: "William",
+        avatar: "",
+      },
+      category: "Default",
+      published: true,
+      dateCreated: this.date,
+      datePublished: this.date,
+      lastModified: this.date,
+    },
+  ];
+}
 
-  ngOnInit(): void {
-  }
+interface PageContent {
+  title: string;
+  section: string;
+  content: {
+    image: boolean;
+    text: string;
+  };
+}
 
+interface BlogPost {
+  title: string;
+  intro: string;
+  content: string;
+  image: {
+    url: string;
+    description: string;
+    alt: string;
+    filename: string;
+  };
+  author: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
+  category: string;
+  published: boolean;
+  dateCreated: Date;
+  datePublished: Date;
+  lastModified: Date;
 }
