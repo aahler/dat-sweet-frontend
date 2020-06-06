2df6dc769d103d89f16c28ceec34cbbe0e93aae4
aahler <53316736+aahler@users.noreply.github.com>
2020-06-06 13:34:14 -0400

    Filter by category

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,9 +11,12 @@ export class HomeComponent implements OnInit {
   constructor() {}
 
   blogPosts: BlogPost[];
+  allBlogPosts: BlogPost[];
   data: BlogPost = null;
   opened: boolean = false;
+  status: boolean = false;
   index: number;
+  categories: string[] = [];
 
   delete(i: BlogPost) {
     const index = this.blogPosts.indexOf(i, 0);
@@ -22,17 +25,44 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  clickEvent() {
+    this.status = !this.status;
+  }
+
+  filter(category) {
+    if (category === "All") {
+      this.blogPosts = this.allBlogPosts;
+    } else {
+      this.blogPosts = this.allBlogPosts.filter((x) => x.category == category);
+    }
+  }
+
   openModal() {
     this.opened = !this.opened;
   }
+
   edit(i: BlogPost, e: number) {
     this.data = i;
     this.index = e;
     this.openModal();
   }
 
+  setCategories() {
+    this.allBlogPosts.map((x) => {
+      return this.categories.includes(x.category)
+        ? null
+        : this.categories.push(x.category);
+    });
+  }
+
+  setBlogPosts() {
+    this.allBlogPosts = FakeData.blogPosts;
+    this.blogPosts = this.allBlogPosts;
+    this.setCategories();
+  }
+
   ngOnInit(): void {
-    this.blogPosts = FakeData.blogPosts;
+    this.setBlogPosts();
   }
 }
 
--- src/assets/fakeBlogPosts.ts
+++ src/assets/fakeBlogPosts.ts
@@ -5,9 +5,10 @@ export class FakeData {
     {
       title: "Test title",
       intro: "Test Intro",
-      content: "This is test content for the blog posts",
+      content:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
       image: {
-        url: "",
+        url: "https://picsum.photos/640/360",
         description: "Description of the image",
         alt: "Test picture",
         filename: "Test Picture",
@@ -15,7 +16,7 @@ export class FakeData {
       author: {
         id: "12345",
         name: "Greg",
-        avatar: "",
+        avatar: "https://picsum.photos/48/48",
       },
       category: "Default",
       published: false,
@@ -28,7 +29,7 @@ export class FakeData {
       intro: "Test Intro",
       content: "This is test content for the blog posts",
       image: {
-        url: "",
+        url: "https://picsum.photos/640/360",
         description: "Description of the image",
         alt: "Test picture",
         filename: "Test Picture",
@@ -36,9 +37,9 @@ export class FakeData {
       author: {
         id: "123456",
         name: "Austin",
-        avatar: "",
+        avatar: "https://picsum.photos/48/48",
       },
-      category: "Default",
+      category: "Fitness",
       published: true,
       dateCreated: new Date(),
       datePublished: new Date(),
@@ -49,7 +50,7 @@ export class FakeData {
       intro: "Test Intro",
       content: "This is test content for the blog posts",
       image: {
-        url: "",
+        url: "https://picsum.photos/640/360",
         description: "Description of the image",
         alt: "Test picture",
         filename: "Test Picture",
@@ -57,7 +58,7 @@ export class FakeData {
       author: {
         id: "1234567",
         name: "Joey",
-        avatar: "",
+        avatar: "https://picsum.photos/48/48",
       },
       category: "Default",
       published: true,
@@ -70,7 +71,7 @@ export class FakeData {
       intro: "Test Intro",
       content: "This is test content for the blog posts",
       image: {
-        url: "",
+        url: "https://picsum.photos/640/360",
         description: "Description of the image",
         alt: "Test picture",
         filename: "Test Picture",
@@ -78,12 +79,12 @@ export class FakeData {
       author: {
         id: "12345678",
         name: "William",
-        avatar: "",
+        avatar: "https://picsum.photos/48/48",
       },
       category: "Default",
-      published: true,
-      dateCreated: new Date(),
-      datePublished: new Date(),
+      published: false,
+      dateCreated: new Date(-2),
+      datePublished: null,
       lastModified: new Date(),
     },
   ];
